3222191b91974c8aef11680985d67aa298f68d34
Ryan Murry <ryanjmurry@gmail.com>
2018-08-17 16:47:01 -0700

    create new pipe and add logic

---
+++ src/app/game-level.pipe.ts
@@ -0,0 +1,29 @@
+import {Pipe, PipeTransform} from '@angular/core';
+import { Game } from './models/game.model';
+
+@Pipe({
+  name: "gamelevel",
+  pure: false
+})
+
+export class GameLevelPipe implements PipeTransform {
+  transform(input: Game[], level) {
+    var output: Game[] = [];
+    if(level === "V") {
+      for (let i = 0; i < input.length; i++) {
+        if (input[i].level === "V") {
+          output.push(input[i]);
+        }
+      }
+      return output;
+    }
+    else {
+      for (let i = 0; i < input.length; i++) {
+        if (input[i].level === "JV") {
+          output.push(input[i]);
+        }
+      }
+      return output;
+    }
+  }
+}
