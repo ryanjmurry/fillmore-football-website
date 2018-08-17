c11eb3e09c8da4e96b40d73d1eed7d7eca3faba6
Ryan Murry <ryanjmurry@gmail.com>
2018-08-17 12:29:54 -0700

    add roster component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ScheduleComponent } from './schedule/schedule.component';
 import { CoachesComponent } from './coaches/coaches.component';
+import { RosterComponent } from './roster/roster.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ScheduleComponent,
-    CoachesComponent
+    CoachesComponent,
+    RosterComponent
   ],
   imports: [
     BrowserModule
