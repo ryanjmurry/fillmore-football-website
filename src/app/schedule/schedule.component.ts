f6514a50a37ea1657a28b71bcbb8e90c866dd1c7
Ryan Murry <ryanjmurry@gmail.com>
2018-08-17 15:26:49 -0700

    inject mock games into schedule component

--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from '../models/game.model';
+import { GameService } from '../game.service';
 
 @Component({
   selector: 'app-schedule',
   templateUrl: './schedule.component.html',
-  styleUrls: ['./schedule.component.css']
+  styleUrls: ['./schedule.component.css'],
+  providers: [GameService]
 })
 export class ScheduleComponent implements OnInit {
+  games: Game[];
 
-  constructor() { }
+  constructor(private gameService: GameService) { }
 
   ngOnInit() {
+    this.games = this.gameService.getGames();
   }
 
 }
