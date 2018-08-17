f2b0af57115c493c947e4e546980663724f8399f
Ryan Murry <ryanjmurry@gmail.com>
2018-08-17 14:33:56 -0700

    add constructor and export class

---
+++ src/app/models/coach.model.ts
@@ -0,0 +1,10 @@
+export class Coach {
+  constructor(
+    public name: string,
+    public picture: string,
+    public email: string,
+    public level: string,
+    public role: string,
+    public bio: string
+  ) {}
+}
