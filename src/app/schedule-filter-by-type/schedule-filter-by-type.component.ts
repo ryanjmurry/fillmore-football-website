e6552000f3074fd19fac2993d6d28f1ad5cfd945
Ryan Murry <ryanjmurry@gmail.com>
2018-08-17 19:05:02 -0700

    add output, new eventEmitter, and function to handle changes

--- src/app/schedule-filter-by-type/schedule-filter-by-type.component.ts
+++ src/app/schedule-filter-by-type/schedule-filter-by-type.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-schedule-filter-by-type',
   templateUrl: './schedule-filter-by-type.component.html',
   styleUrls: ['./schedule-filter-by-type.component.css']
 })
-export class ScheduleFilterByTypeComponent implements OnInit {
+export class ScheduleFilterByTypeComponent {
+  @Output() changeSender = new EventEmitter();
 
-  constructor() { }
-
-  ngOnInit() {
+  onChange(gameTypeOption: string) {
+    this.changeSender.emit(gameTypeOption);
   }
-
 }
