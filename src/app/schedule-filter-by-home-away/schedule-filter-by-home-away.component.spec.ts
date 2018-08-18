af8f90668604d94de0516ca5967eca940bc1b8c4
Ryan Murry <ryanjmurry@gmail.com>
2018-08-17 19:03:47 -0700

    fix typo in variables

--- src/app/schedule-filter-by-home-away/schedule-filter-by-home-away.component.spec.ts
+++ src/app/schedule-filter-by-home-away/schedule-filter-by-home-away.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ScheduleFilterByHomeAway } from './schedule-filter-by-home-away.component';
+import { ScheduleFilterByHomeAwayComponent } from './schedule-filter-by-home-away.component';
 
 describe('ScheduleFilterByHomeAway', () => {
-  let component: ScheduleFilterByHomeAway;
-  let fixture: ComponentFixture<ScheduleFilterByHomeAway>;
+  let component: ScheduleFilterByHomeAwayComponent;
+  let fixture: ComponentFixture<ScheduleFilterByHomeAwayComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ScheduleFilterByHomeAway ]
+      declarations: [ ScheduleFilterByHomeAwayComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ScheduleFilterByHomeAway);
+    fixture = TestBed.createComponent(ScheduleFilterByHomeAwayComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
