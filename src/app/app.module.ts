4e79a6c58175460afa1b90c5cb1ba7989da759c8
Ryan Murry <ryanjmurry@gmail.com>
2018-08-21 00:36:09 -0700

    set up firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
+
 import { AppComponent } from './app.component';
 import { ScheduleComponent } from './schedule/schedule.component';
 import { CoachesComponent } from './coaches/coaches.component';
@@ -11,15 +12,25 @@ import { DocumentsComponent } from './documents/documents.component';
 import { LoginComponent } from './login/login.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ScheduleListComponent } from './schedule-list/schedule-list.component';
-import { GameLevelPipe } from './game-level.pipe';
 import { ScheduleFilterByLevelComponent } from './schedule-filter-by-level/schedule-filter-by-level.component';
 import { ScheduleFilterByHomeAwayComponent } from './schedule-filter-by-home-away/schedule-filter-by-home-away.component';
-import { GameHomeAwayPipe } from './game-home-away.pipe';
 import { ScheduleFilterByTypeComponent } from './schedule-filter-by-type/schedule-filter-by-type.component';
-import { GameTypePipe } from './game-type.pipe';
 import { ScheduleEditComponent } from './schedule-edit/schedule-edit.component';
 
+import { GameLevelPipe } from './game-level.pipe';
+import { GameHomeAwayPipe } from './game-home-away.pipe';
+import { GameTypePipe } from './game-type.pipe';
 
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket,
+};
 
 @NgModule({
   declarations: [
@@ -43,7 +54,9 @@ import { ScheduleEditComponent } from './schedule-edit/schedule-edit.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AngularFontAwesomeModule
+    AngularFontAwesomeModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
