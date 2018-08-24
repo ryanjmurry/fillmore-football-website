42f56247d69fcbe2dc2cb12f3e87a7e577316ac4
Ryan Murry <ryanjmurry@gmail.com>
2018-08-24 13:14:06 -0700

    add method to edit coach details and input property binding

--- src/app/components/coaches/coaches-edit/coaches-edit.component.ts
+++ src/app/components/coaches/coaches-edit/coaches-edit.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CoachService } from '../../../services/coach.service';
 
 @Component({
   selector: 'app-coaches-edit',
   templateUrl: './coaches-edit.component.html',
-  styleUrls: ['./coaches-edit.component.css']
+  styleUrls: ['./coaches-edit.component.css'],
+  providers: [CoachService]
 })
-export class CoachesEditComponent implements OnInit {
+export class CoachesEditComponent {
+  @Input() selectedCoachToEdit;
 
-  constructor() { }
+  constructor(private coachService: CoachService) {}
 
-  ngOnInit() {
+  beginUpdatingCoach(coachToUpdate) {
+    this.coachService.updateCoach(coachToUpdate);
   }
 
 }
