7348e146593f0c7d4ea38772886a4eec4aa39e6d
Ryan Murry <ryanjmurry@gmail.com>
2018-08-24 10:24:54 -0700

    add pipe to format how time appears

---
+++ src/app/time.pipe.ts
@@ -0,0 +1,26 @@
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({
+  name: "time",
+  pure: false
+})
+
+export class TimePipe implements PipeTransform {
+  transform(input: string) {
+    if (input) {
+      let hours = parseInt(input.split(":")[0]);
+      let min = input.split(":")[1];
+      if (hours === 0) {
+        hours += 12
+        return (hours.toString() + ":" + min + "AM");
+      } else if (hours === 12) {
+        return (hours.toString() + ":" + min + "PM");
+      } else if (hours > 12) {
+        hours -= 12;
+        return (hours.toString() + ":" + min + "PM");
+      } else {
+        return (hours.toString() + ":" + min + "AM");
+      }
+    }
+  }
+}
