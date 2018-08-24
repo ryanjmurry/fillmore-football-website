fdd583c92cc00f47473d6670bdfc0810c8d5fa05
Ryan Murry <ryanjmurry@gmail.com>
2018-08-24 12:20:15 -0700

    update coach component with edit and delete methods

--- src/app/components/coaches/coaches.component.ts
+++ src/app/components/coaches/coaches.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { CoachService } from '../../services/coach.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-coaches',
   templateUrl: './coaches.component.html',
-  styleUrls: ['./coaches.component.css']
+  styleUrls: ['./coaches.component.css'],
+  providers: [CoachService]
 })
 export class CoachesComponent implements OnInit {
+  coaches;
+  editCoachClicked: boolean = false;
+  selectedCoachToEdit;
 
-  constructor() { }
+  constructor(private coachService: CoachService, private router: Router) { }
 
   ngOnInit() {
+    this.coachService.getCoaches().subscribe(coaches => {
+      this.coaches = coaches;
+    })
+  }
+
+  editCoachClickedToggle(coachToEdit) {
+    this.editCoachClicked = !this.editCoachClicked;
+    this.selectedCoachToEdit = coachToEdit;
+  }
+
+  deleteCoach(coachToDelete) {
+    if (confirm("Do you really want to delete"))
+    this.coachService.deleteCoach(coachToDelete);
   }
 
 }
