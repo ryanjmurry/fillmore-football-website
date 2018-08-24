8c7d32edf836a695d1274ecb51e235fe8c92bcd0
Ryan Murry <ryanjmurry@gmail.com>
2018-08-24 12:50:19 -0700

    remove picture property from constructor

--- src/app/models/coach.model.ts
+++ src/app/models/coach.model.ts
@@ -1,10 +1,11 @@
 export class Coach {
   constructor(
     public name: string,
-    // public picture: string,
     public email: string,
     public level: string,
     public title: string,
     public bio: string
   ) {}
 }
+
+// public picture: string,
