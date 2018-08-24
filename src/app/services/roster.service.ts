26b638f2fa8a8f5ff9b2ea43fc877e029ba2d7d9
Ryan Murry <ryanjmurry@gmail.com>
2018-08-24 13:44:09 -0700

    update roster service with crud functionality

--- src/app/services/roster.service.ts
+++ src/app/services/roster.service.ts
@@ -1,8 +1,39 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { Player } from '../models/player.model';
 
 @Injectable()
-export class RosterService {
+export class PlayerService {
+  players: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private db: AngularFireDatabase) { 
+    this.players = db.list('players');
+  }
 
+  getPlayeres() {
+    return this.players;
+  }
+
+  addPlayer(newPlayer: Player) {
+    this.players.push(newPlayer);
+  }
+
+  getPlayerById(playerId: string) {
+    return this.db.object('players/' + playerId);
+  }
+
+  updatePlayer(localUpdatedPlayer) {
+    let playerInDb = this.getPlayerById(localUpdatedPlayer.$key);
+    playerInDb.update({name: localUpdatedPlayer.name,
+                    email: localUpdatedPlayer.email,
+                    level: localUpdatedPlayer.level,
+                    title: localUpdatedPlayer.title,
+                    bio: localUpdatedPlayer.bio
+    })
+  }
+
+  deletePlayer(localPlayerToDelete) {
+    let playerInDb = this.getPlayerById(localPlayerToDelete.$key);
+    playerInDb.remove();
+  }
 }
