b145eb4441690043646e5c79aba6d31317f4b158
Ryan Murry <ryanjmurry@gmail.com>
2018-08-24 12:50:50 -0700

    add new coach to db once form is submitted

--- src/app/components/coaches/coaches-add/coaches-add.component.ts
+++ src/app/components/coaches/coaches-add/coaches-add.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Coach } from '../../../models/coach.model';
+import { CoachService } from '../../../services/coach.service';
 
 @Component({
   selector: 'app-coaches-add',
   templateUrl: './coaches-add.component.html',
-  styleUrls: ['./coaches-add.component.css']
+  styleUrls: ['./coaches-add.component.css'],
+  providers: [CoachService]
 })
-export class CoachesAddComponent implements OnInit {
+export class CoachesAddComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private coachService: CoachService) { }
+  
+  submitForm(levelInput, nameInput, emailInput, titleInput, bioInput) {
+    let newCoach = new Coach(nameInput, emailInput, levelInput, titleInput, bioInput)
+    this.coachService.addCoach(newCoach);
   }
-
 }
