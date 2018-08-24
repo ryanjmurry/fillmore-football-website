5fc5e7891c43842a1454357941f3054fea362808
Ryan Murry <ryanjmurry@gmail.com>
2018-08-24 14:40:05 -0700

    add roster service and crud methods

--- src/app/components/roster/roster.component.ts
+++ src/app/components/roster/roster.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { RosterService } from '../../services/roster.service';
 
 @Component({
   selector: 'app-roster',
   templateUrl: './roster.component.html',
-  styleUrls: ['./roster.component.css']
+  styleUrls: ['./roster.component.css'],
+  providers: [RosterService]
 })
 export class RosterComponent implements OnInit {
+  players;
+  editPlayerClicked: boolean = false;
+  selectedPlayerToEdit;
 
-  constructor() { }
+  constructor(private rosterService: RosterService) { }
 
   ngOnInit() {
+    this.rosterService.getPlayers().subscribe(roster => {
+      this.players = roster;
+    })
+  }
+
+  editPlayerClickedToggle(playerToEdit) {
+    this.editPlayerClicked = !this.editPlayerClicked;
+    this.selectedPlayerToEdit = playerToEdit;
+  }
+
+  deletePlayer(playerToDelete) {
+    if (confirm("Do you really want to delete"))
+    this.rosterService.deletePlayer(playerToDelete);
   }
 
 }
+
