63c1305f9a2049ba231462309f597631fc637f92
Ryan Murry <ryanjmurry@gmail.com>
2018-08-24 03:24:59 -0700

    add logic to load page with values from database

--- src/app/schedule-edit/schedule-edit.component.ts
+++ src/app/schedule-edit/schedule-edit.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { Game } from '../models/game.model';
+import { GameService } from '../services/game.service';
 
 @Component({
   selector: 'app-schedule-edit',
   templateUrl: './schedule-edit.component.html',
-  styleUrls: ['./schedule-edit.component.css']
+  styleUrls: ['./schedule-edit.component.css'],
+  providers: [GameService]
 })
 export class ScheduleEditComponent implements OnInit {
+  gameId: string;
+  gameToDisplay;
+  htmlTimeFormat;
+  htmlDateFormat;
 
-  constructor() { }
+  constructor(private gameService: GameService, private route: ActivatedRoute, private location: Location) {}
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+      this.gameId = urlParameters['id'];
+    })
+
+    this.gameService.getGameById(this.gameId).subscribe(game => {
+      this.gameToDisplay = game;
+      this.setHtmlDateAndTime(this.gameToDisplay.date, this.gameToDisplay.time);
+    })
   }
 
+  setHtmlDateAndTime(date: string, time: string) {
+    let newHtmlDate = new Date(date + " " + time);
+    let year = newHtmlDate.getFullYear();
+    let month = ('0' + (newHtmlDate.getMonth() + 1)).slice(-2);
+    let day = ('0' + newHtmlDate.getDate()).slice(-2);
+    let hour = newHtmlDate.getHours();
+    let min = ('0' + newHtmlDate.getMinutes()).slice(-2);
+    this.htmlDateFormat = `${year}-${month}-${day}`;
+    this.htmlTimeFormat = `${hour}:${min}`
+  }
+
+  toHtmlTime(time: string) {
+    let newHtmlTime = new Date(time);
+    console.log(newHtmlTime);
+  }
 }
