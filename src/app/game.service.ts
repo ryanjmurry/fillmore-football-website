317c57d0f9f908f67fce33516dd1eaa29fb2fd8d
Ryan Murry <ryanjmurry@gmail.com>
2018-08-23 22:36:15 -0700

    set up game service to use databse data

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Game } from './models/game.model';
-import { GAMES } from './mock-games';
+// import { GAMES } from './mock-games';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Injectable()
 export class GameService {
+  games: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private database: AngularFireDatabase) { 
+    this.games = database.list('games');
+  }
 
   getGames() {
-    return GAMES;
+    return this.games;
   }
+
+  // getGames() {
+  //   return GAMES;
+  // }
 
 }
