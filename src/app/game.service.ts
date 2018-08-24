6f1da91b9dfb6a7c062c5f51e904926e5c07cb4f
Ryan Murry <ryanjmurry@gmail.com>
2018-08-23 23:06:59 -0700

    add method to add game to database

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { Game } from './models/game.model';
 
 @Injectable()
 export class GameService {
@@ -11,5 +12,13 @@ export class GameService {
 
   getGames() {
     return this.games;
+  }
+
+  addGame(newGame: Game) {
+    this.games.push(newGame);
+  }
+
+  getGamesById(gameOfInterest: ) {
+
   }
 }
