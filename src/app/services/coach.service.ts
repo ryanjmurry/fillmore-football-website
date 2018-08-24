df5fea1f5edea92d1d4b0ec41fcd3d70d34036b8
Ryan Murry <ryanjmurry@gmail.com>
2018-08-24 11:45:39 -0700

    add import firedatabase and list observable and create crud methods

--- src/app/services/coach.service.ts
+++ src/app/services/coach.service.ts
@@ -1,8 +1,39 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { Coach } from '../models/coach.model';
 
 @Injectable()
 export class CoachService {
+  coaches: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private db: AngularFireDatabase) { 
+    this.coaches = db.list('coaches');
+  }
 
+  getCoaches() {
+    return this.coaches;
+  }
+
+  addCoach(newCoach: Coach) {
+    this.coaches.push(newCoach);
+  }
+
+  getCoachById(coachId: string) {
+    return this.db.object('coaches/' + coachId);
+  }
+
+  updateCoach(localUpdatedCoach) {
+    let coachInDb = this.getCoachById(localUpdatedCoach.$key);
+    coachInDb.update({date: localUpdatedCoach.name,
+                    homeAway: localUpdatedCoach.email,
+                    level: localUpdatedCoach.level,
+                    location: localUpdatedCoach.title,
+                    opponent: localUpdatedCoach.bio
+    })
+  }
+
+  deleteCoach(localCoachToDelete) {
+    let coachInDb = this.getCoachById(localCoachToDelete.$key);
+    coachInDb.remove();
+  }
 }
