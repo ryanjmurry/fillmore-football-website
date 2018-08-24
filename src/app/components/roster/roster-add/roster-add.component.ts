56c127f1ee1fb72c65b3666f4ecce5ed05bb17a1
Ryan Murry <ryanjmurry@gmail.com>
2018-08-24 14:40:28 -0700

    add roster service and method to add new player

--- src/app/components/roster/roster-add/roster-add.component.ts
+++ src/app/components/roster/roster-add/roster-add.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Player } from '../../../models/player.model';
+import { RosterService } from '../../../services/roster.service';
 
 @Component({
   selector: 'app-roster-add',
   templateUrl: './roster-add.component.html',
-  styleUrls: ['./roster-add.component.css']
+  styleUrls: ['./roster-add.component.css'],
+  providers: [RosterService]
 })
-export class RosterAddComponent implements OnInit {
+export class RosterAddComponent {
 
-  constructor() { }
+  constructor(private rosterService: RosterService) {}
 
-  ngOnInit() {
+  submitForm(levelInput, firstNameInput, lastNameInput, numberInput, offPositionInput, defPositionInput, gradeInput) {
+    let newPlayer = new Player(firstNameInput, lastNameInput, numberInput, offPositionInput, defPositionInput, gradeInput, levelInput);
+    this.rosterService.addPlayer(newPlayer);
   }
-
 }
