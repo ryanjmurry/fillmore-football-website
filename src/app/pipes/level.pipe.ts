b313a44f0e2635e39ae1e253e47674a4fbe58461
Ryan Murry <ryanjmurry@gmail.com>
2018-08-26 00:32:48 -0700

    refactor pipe

---
+++ src/app/pipes/level.pipe.ts
@@ -0,0 +1,29 @@
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({
+  name: "level",
+  pure: false
+})
+
+export class LevelPipe implements PipeTransform {
+  transform(input, level) {
+    var output = [];
+    if (input) {
+      if(level === "V") {
+        for (let i = 0; i < input.length; i++) {
+          if (input[i].level === "V") {
+            output.push(input[i]);
+          }
+        }
+        return output;
+      } else {
+        for (let i = 0; i < input.length; i++) {
+          if (input[i].level === "JV") {
+            output.push(input[i]);
+          }
+        }
+        return output;
+      }
+    }
+  }
+}
